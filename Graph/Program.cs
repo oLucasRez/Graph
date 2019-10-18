55011f2a8db0bcffb4ba3a1552c6ee45fe67cbf0
Lucas Rezende <arrobagemeioponto@gmail.com>
2019-10-17 21:24:12 -0300

    a

--- Graph/Program.cs
+++ Graph/Program.cs
@@ -1,110 +1,123 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 
 namespace Graph
 {
-    public class Graph<T>
-    {
-        protected Dictionary<T, List<Node>> LdA = new Dictionary<T, List<Node>>();
-        protected int nVertex = 0;
+    //public class Graph<T>
+    //{
+    //    protected Dictionary<T, List<Node>> LdA = new Dictionary<T, List<Node>>();
+    //    protected int nVertex = 0;
 
-        protected class Node
-        {
-            public T content;
-            public double weight;
-            public Node(T value, double weight)
-            {
-                content = value;
-                this.weight = weight;
-            }
-        }
+    //    protected class Node
+    //    {
+    //        public T content;
+    //        public double weight;
+    //        public Node(T value, double weight)
+    //        {
+    //            content = value;
+    //            this.weight = weight;
+    //        }
+    //    }
 
-        public void AddNode(T value)
-        {
-            foreach (T key in LdA.Keys) if (key.Equals(value)) return;
-            LdA.Add(value, new List<Node>());
-            nVertex++;
-        }
+    //    public void AddNode(T value)
+    //    {
+    //        foreach (T key in LdA.Keys) if (key.Equals(value)) return;
+    //        LdA.Add(value, new List<Node>());
+    //        nVertex++;
+    //    }
 
-        Dictionary<T, bool> open;
-        public List<T> DFS(T start)
-        {
-            open = new Dictionary<T, bool>();
-            List<List<T>> lists = new List<List<T>>();
-            foreach (T k in LdA.Keys) open.Add(k, true);
-            return DFSVisit(start, null);
-        }
+    //    Dictionary<T, bool> open;
+    //    public List<T> DFS(T start)
+    //    {
+    //        open = new Dictionary<T, bool>();
+    //        List<List<T>> lists = new List<List<T>>();
+    //        foreach (T k in LdA.Keys) open.Add(k, true);
+    //        return DFSVisit(start, null);
+    //    }
 
-        List<T> DFSVisit(T current, List<T> list)
-        {
-            open[current] = false;
-            if (list == null) list = new List<T>();
-            list.Add(current);
-            foreach (Node next in LdA[current])
-                if (open[next.content]) DFSVisit(next.content, list);
-            open[current] = false;
-            return list;
-        }
+    //    List<T> DFSVisit(T current, List<T> list)
+    //    {
+    //        open[current] = false;
+    //        if (list == null) list = new List<T>();
+    //        list.Add(current);
+    //        foreach (Node next in LdA[current])
+    //            if (open[next.content]) DFSVisit(next.content, list);
+    //        open[current] = false;
+    //        return list;
+    //    }
 
-        public List<List<T>> DFS()
-        {
-            List<List<T>> lists = new List<List<T>>();
-            foreach (T key in LdA.Keys) lists.Add(DFS(key));
-            return lists;
-        }
+    //    public List<List<T>> DFS()
+    //    {
+    //        List<List<T>> lists = new List<List<T>>();
+    //        foreach (T key in LdA.Keys) lists.Add(DFS(key));
+    //        return lists;
+    //    }
 
-        public void AddEdge(T value1, T value2)
-        {
-            if (LdA.ContainsKey(value1) && LdA.ContainsKey(value2))
-            {
-                foreach (Node node in LdA[value1]) if (node.content.Equals(value2)) return;
-                LdA[value1].Add(new Node(value2, 1));
-                LdA[value2].Add(new Node(value1, 1));
-            }
-        }
-    }
+    //    public void AddEdge(T value1, T value2)
+    //    {
+    //        if (LdA.ContainsKey(value1) && LdA.ContainsKey(value2))
+    //        {
+    //            foreach (Node node in LdA[value1]) if (node.content.Equals(value2)) return;
+    //            LdA[value1].Add(new Node(value2, 1));
+    //            LdA[value2].Add(new Node(value1, 1));
+    //        }
+    //    }
+    //}
 
     class Program
     {
         static void Main(string[] args)
         {
-            int mudRoads = 0;
-            int nCases = int.Parse(Console.ReadLine());
-            for (int i = 1; i <= nCases; i++)
+            int col = int.Parse(Console.ReadLine());
+            int row = int.Parse(Console.ReadLine());
+            char[][] matrix = new char[row][];
+            for (int i = 0; i < row; i++)
+                matrix[i] = Console.ReadLine().ToCharArray();
+            Dictionary<char, Vector2> direction = new Dictionary<char, Vector2>(){
+                {'^', new Vector2(-1, 0) },
+                {'v', new Vector2(1, 0) },
+                {'>', new Vector2(0, 1) },
+                {'<', new Vector2(0, -1) }
+            };
+            Vector2 me = new Vector2(0, 0);
+            char c = matrix[(int)me.X][(int)me.Y];
+            List<Vector2> visited = new List<Vector2>();
+            char lastDirect = '.';
+            char resp = ' ';
+            while (c != '*')
             {
-                Graph<int> graph = new Graph<int>();
-                int nV = int.Parse(Console.ReadLine());
-                int nE = int.Parse(Console.ReadLine());
-                for (int j = 1; j <= nV; j++) graph.AddNode(j);
-                for (int j = 0; j < nE; j++)
+                if (c != '.')
                 {
-                    string[] edge = Console.ReadLine().Split();
-                    graph.AddEdge(int.Parse(edge[0]), int.Parse(edge[1]));
-                }
-                var dfs = graph.DFS();
-                int nIslands = 0;
-                List<int> visit = new List<int>();
-                foreach (var list in dfs)
-                {
-                    Func<bool> contain = () =>
+                    lastDirect = c;
+                    var a = new Vector2(me.X, me.Y);
+                    if (visited.Where(x => x.X == a.X && x.Y == a.Y).Count() == 0)
                     {
-                        foreach (var visited in visit)
-                        {
-                            if (list.Contains(visited)) return true;
-                        }
-                        return false;
-                    };
-                    if (list.Count < nV && !contain())
-                    {
-                        nIslands++;
-                        visit.Add(list.First());
+                        visited.Add(a);
                     }
+                    else
+                    {
+                        resp = '!';
+                        break;
+                    }
+                    me.X += direction[c].X;
+                    me.Y += direction[c].Y;
                 }
-                mudRoads = nIslands - 1;
-                if (mudRoads == -1) Console.WriteLine("Caso #" + i + ": a promessa foi cumprida");
-                else Console.WriteLine("Caso #" + i + ": ainda falta(m) " + mudRoads + " estrada(s)");
+                else
+                {
+                    me.X += direction[lastDirect].X;
+                    me.Y += direction[lastDirect].Y;
+                }
+                if(me.X < 0 || me.X >= row || me.Y < 0 || me.Y >= col)
+                {
+                    resp = '!';
+                    break;
+                }
+                c = matrix[(int)me.X][(int)me.Y];
             }
+            if (resp == ' ') Console.WriteLine("*");
+            else Console.WriteLine(resp);
         }
     }
 }
